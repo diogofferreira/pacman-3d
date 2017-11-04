62ede3669a11a1757566019fffe5e3ea84208e8f
Diogo Ferreira <pdiogoferreira23@gmail.com>
2017-11-04 02:52:12 +0000

    Added initial files. Just a sphere walking around

---
+++ Pacman-3D.js
@@ -0,0 +1,464 @@
+//----------------------------------------------------------------------------
+//
+// Global Variables
+//
+
+var gl = null; // WebGL context
+
+var shaderProgram = null;
+
+var triangleVertexPositionBuffer = null;
+	
+var triangleVertexColorBuffer = null;
+
+// The local transformation parameters
+
+// The translation vector
+
+var tx = 0.0;
+
+var ty = 0.0;
+
+var tz = 0.0;
+
+// The rotation angles in degrees
+
+var angleXX = 0.0;
+
+var angleYY = 0.0;
+
+var angleZZ = 0.0;
+
+// The scaling factors
+
+var sx = 0.5;
+
+var sy = 0.5;
+
+var sz = 0.5;
+
+
+// For storing the vertices defining the triangles
+
+var vertices = [
+
+		// FRONT FACE
+		 
+		-0.25, -0.25,  0.25,
+		 
+		 0.25, -0.25,  0.25,
+		 
+		 0.25,  0.25,  0.25,
+
+		 
+		 0.25,  0.25,  0.25,
+		 
+		-0.25,  0.25,  0.25,
+		 
+		-0.25, -0.25,  0.25,
+		
+		// TOP FACE
+		
+		-0.25,  0.25,  0.25,
+		 
+		 0.25,  0.25,  0.25,
+		 
+		 0.25,  0.25, -0.25,
+
+		 
+		 0.25,  0.25, -0.25,
+		 
+		-0.25,  0.25, -0.25,
+		 
+		-0.25,  0.25,  0.25,
+		
+		// BOTTOM FACE 
+		
+		-0.25, -0.25, -0.25,
+		 
+		 0.25, -0.25, -0.25,
+		 
+		 0.25, -0.25,  0.25,
+
+		 
+		 0.25, -0.25,  0.25,
+		 
+		-0.25, -0.25,  0.25,
+		 
+		-0.25, -0.25, -0.25,
+		
+		// LEFT FACE 
+		
+		-0.25,  0.25,  0.25,
+		 
+		-0.25, -0.25, -0.25,
+
+		-0.25, -0.25,  0.25,
+		 
+		 
+		-0.25,  0.25,  0.25,
+		 
+		-0.25,  0.25, -0.25,
+		 
+		-0.25, -0.25, -0.25,
+		
+		// RIGHT FACE 
+		
+		 0.25,  0.25, -0.25,
+		 
+		 0.25, -0.25,  0.25,
+
+		 0.25, -0.25, -0.25,
+		 
+		 
+		 0.25,  0.25, -0.25,
+		 
+		 0.25,  0.25,  0.25,
+		 
+		 0.25, -0.25,  0.25,
+		
+		// BACK FACE 
+		
+		-0.25,  0.25, -0.25,
+		 
+		 0.25, -0.25, -0.25,
+
+		-0.25, -0.25, -0.25,
+		 
+		 
+		-0.25,  0.25, -0.25,
+		 
+		 0.25,  0.25, -0.25,
+		 
+		 0.25, -0.25, -0.25,			 
+];
+
+// And their colour
+
+var colors = [
+
+		 // FRONT FACE
+		 	
+		 1.00,  0.00,  1.00,
+		 
+		 1.00,  0.00,  1.00,
+		 
+		 1.00,  0.00,  1.00,
+
+		 	
+		 1.00,  0.00,  1.00,
+		 
+		 1.00,  0.00,  1.00,
+		 
+		 1.00,  0.00,  1.00,
+		 			 
+		 // TOP FACE
+		 	
+		 1.00,  0.00,  0.00,
+		 
+		 1.00,  0.00,  0.00,
+		 
+		 1.00,  0.00,  0.00,
+
+		 	
+		 1.00,  0.00,  0.00,
+		 
+		 1.00,  0.00,  0.00,
+		 
+		 1.00,  0.00,  0.00,
+		 			 
+		 // BOTTOM FACE
+		 	
+		 1.00,  0.00,  1.00,
+		 
+		 1.00,  0.00,  1.00,
+		 
+		 1.00,  0.00,  1.00,
+
+		 	
+		 1.00,  0.00,  1.00,
+		 
+		 1.00,  0.00,  1.00,
+		 
+		 1.00,  0.00,  1.00,
+		 			 
+		 // LEFT FACE
+		 	
+		 1.00,  0.00,  0.00,
+		 
+		 1.00,  0.00,  0.00,
+		 
+		 1.00,  0.00,  0.00,
+
+		 	
+		 1.00,  0.00,  0.00,
+		 
+		 1.00,  0.00,  0.00,
+		 
+		 1.00,  0.00,  0.00,
+		 			 
+		 // RIGHT FACE
+		 	
+		 1.00,  0.00,  1.00,
+		 
+		 1.00,  0.00,  1.00,
+		 
+		 1.00,  0.00,  1.00,
+
+		 	
+		 1.00,  0.00,  1.00,
+		 
+		 1.00,  0.00,  1.00,
+		 
+		 1.00,  0.00,  1.00,
+		 			 
+		 			 
+		 // BACK FACE
+		 	
+		 1.00,  0.00,  0.00,
+		 
+		 1.00,  0.00,  0.00,
+		 
+		 1.00,  0.00,  0.00,
+
+		 	
+		 1.00,  0.00,  0.00,
+		 
+		 1.00,  0.00,  0.00,
+		 
+		 1.00,  0.00,  0.00,			 			 
+];
+
+//----------------------------------------------------------------------------
+//
+// The WebGL code
+//
+
+//----------------------------------------------------------------------------
+//
+//  Rendering
+//
+
+// Handling the Vertex and the Color Buffers
+
+function initBuffers() {	
+	
+	// Coordinates
+		
+	triangleVertexPositionBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexPositionBuffer);
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+	triangleVertexPositionBuffer.itemSize = 3;
+	triangleVertexPositionBuffer.numItems = vertices.length / 3;			
+
+	// Associating to the vertex shader
+	
+	gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, 
+			triangleVertexPositionBuffer.itemSize, 
+			gl.FLOAT, false, 0, 0);
+	
+	// Colors
+		
+	triangleVertexColorBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexColorBuffer);
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
+	triangleVertexColorBuffer.itemSize = 3;
+	triangleVertexColorBuffer.numItems = colors.length / 3;			
+
+	// Associating to the vertex shader
+	
+	gl.vertexAttribPointer(shaderProgram.vertexColorAttribute, 
+			triangleVertexColorBuffer.itemSize, 
+			gl.FLOAT, false, 0, 0);
+
+}
+
+//----------------------------------------------------------------------------
+
+//  Drawing the model
+
+function drawModel( angleXX, angleYY, angleZZ, 
+					sx, sy, sz,
+					tx, ty, tz,
+					mvMatrix,
+					primitiveType ) {
+
+    // Pay attention to transformation order !!
+    
+	mvMatrix = mult( mvMatrix, translationMatrix( tx, ty, tz ) );
+						 
+	mvMatrix = mult( mvMatrix, rotationZZMatrix( angleZZ ) );
+	
+	mvMatrix = mult( mvMatrix, rotationYYMatrix( angleYY ) );
+	
+	mvMatrix = mult( mvMatrix, rotationXXMatrix( angleXX ) );
+	
+	mvMatrix = mult( mvMatrix, scalingMatrix( sx, sy, sz ) );
+						 
+	// Passing the Model View Matrix to apply the current transformation
+	
+	var mvUniform = gl.getUniformLocation(shaderProgram, "uMVMatrix");
+	
+	gl.uniformMatrix4fv(mvUniform, false, new Float32Array(flatten(mvMatrix)));
+	
+	// Drawing the contents of the vertex buffer
+	
+	gl.drawArrays(primitiveType, 0, triangleVertexPositionBuffer.numItems); 
+	
+}
+
+//----------------------------------------------------------------------------
+
+//  Drawing the 3D scene
+
+function drawScene() {
+	
+	var pMatrix;
+	
+	var mvMatrix = mat4();
+	
+	// Clearing the frame-buffer and the depth-buffer
+	
+	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+	// Computing the perspective matrix
+	
+	pMatrix = perspective( 45, 1, 0.05, 15 );
+	
+	// Global transformation (Fix POV)
+	
+	globalTz = -10.0;
+
+	// Passing the Projection Matrix to apply the current projection
+	
+	var pUniform = gl.getUniformLocation(shaderProgram, "uPMatrix");
+	
+	gl.uniformMatrix4fv(pUniform, false, new Float32Array(flatten(pMatrix)));
+		
+	mvMatrix = translationMatrix( 0, 0, globalTz );
+	
+	// Instantianting the current model
+		
+	drawModel( angleXX, angleYY, angleZZ, 
+	           sx, sy, sz,
+	           tx, ty, tz,
+	           mvMatrix,
+	           primitiveType );
+}
+
+// Timer
+
+function tick() {
+	
+	requestAnimFrame(tick);
+	
+	drawScene();
+}
+
+//----------------------------------------------------------------------------
+
+function setEventListeners(){
+
+	// Pacman Movement
+	document.addEventListener("keydown", function(event){
+		
+		// Getting the pressed key 
+		var key = event.keyCode;
+
+		switch(key){
+			// Left
+			case 37 : // a
+				// Updating
+	
+				tx -= 0.25;
+				
+				// Render the viewport
+				drawScene(); 
+
+				break;
+			// Up
+			case 38 :
+				tz -= 0.50;
+				
+				// Render the viewport
+				drawScene(); 
+
+				break;
+			// Right
+			case 39 :
+				tx += 0.25;
+				
+				// Render the viewport
+				drawScene(); 
+
+				break;
+			// Down
+			case 40 : 
+				tz += 0.50;
+				
+				// Render the viewport
+				drawScene(); 
+
+				break;
+		}
+	});		     
+}
+
+//----------------------------------------------------------------------------
+//
+// WebGL Initialization
+//
+
+function initWebGL( canvas ) {
+	try {
+		
+		// Create the WebGL context
+		
+		// Some browsers still need "experimental-webgl"
+		
+		gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
+		
+		// Drawing the triangles defining the model
+		
+		primitiveType = gl.TRIANGLES;
+				
+		// Enable face culling and depth test
+		
+		gl.enable( gl.CULL_FACE );
+		gl.enable( gl.DEPTH_TEST );
+        
+	} catch (e) {
+	}
+	if (!gl) {
+		alert("Could not initialise WebGL, sorry! :-(");
+	}        
+}
+
+//----------------------------------------------------------------------------
+
+function runWebGL() {
+	
+	var canvas = document.getElementById("my-canvas");
+	
+	initWebGL( canvas );
+
+	shaderProgram = initShaders( gl );
+	
+	setEventListeners();
+	
+	initBuffers();
+
+	// Transform cube into sphere
+
+	centroidRefinement( vertices, colors, 6 );
+    
+    initBuffers();
+
+ 	moveToSphericalSurface( vertices );
+    
+    initBuffers();
+	
+	drawScene();
+
+	//tick();   
+}
---
+++ models.js
@@ -0,0 +1,244 @@
+//////////////////////////////////////////////////////////////////////////////
+//
+//  Functions for processing triangle mesh models
+//
+//  J. Madeira - Oct. 2015
+//
+//////////////////////////////////////////////////////////////////////////////
+
+//----------------------------------------------------------------------------
+//
+//  Recursive triangle subdivision, using the midpoints of edges
+//
+
+function recSubdivisionMidPoint( v1, v2, v3, 
+								 c1, c2, c3,
+								 coordsArray,
+								 colorsArray,
+								 recursionDepth ) {
+
+	// Recursive midpoint subdivision of one triangle
+	
+	if( recursionDepth == 0 ) {
+		
+		// Storing coordinates and colors in the destination arrays
+ 
+        coordsArray.push( v1[0], v1[1], v1[2] );
+		
+		coordsArray.push( v2[0], v2[1], v2[2] );
+		
+		coordsArray.push( v3[0], v3[1], v3[2] );
+		
+		colorsArray.push( c1[0], c1[1], c1[2] );
+		
+		colorsArray.push( c2[0], c2[1], c2[2] );
+		
+		colorsArray.push( c3[0], c3[1], c3[2] );	    
+	}
+	else {
+		
+		// Compute the midpoints and proceed recursively
+		
+        var mid12 = computeMidPoint( v1, v2 );
+
+        var mid23 = computeMidPoint( v2, v3 );
+        
+        var mid31 = computeMidPoint( v3, v1 );
+        
+        // Colors are also averaged
+
+        var c12 = computeMidPoint( c1, c2 );
+
+        var c23 = computeMidPoint( c2, c3 );
+
+        var c31 = computeMidPoint( c3, c1 );
+        
+        // 4 recursive calls 
+
+        recSubdivisionMidPoint( v1, mid12, mid31, c1, c12, c31,
+                                coordsArray, colorsArray, recursionDepth - 1 );
+
+        recSubdivisionMidPoint( v2, mid23, mid12, c2, c23, c12,
+                                coordsArray, colorsArray, recursionDepth - 1 );
+
+        recSubdivisionMidPoint( v3, mid31, mid23, c3, c31, c23,
+                                coordsArray, colorsArray, recursionDepth - 1 );
+
+        recSubdivisionMidPoint( mid12, mid23, mid31, c12, c23, c31,
+                                coordsArray, colorsArray, recursionDepth - 1 );
+	}
+}
+
+//----------------------------------------------------------------------------
+
+function midPointRefinement( coordsArray, 
+						     colorsArray, 
+							 recursionDepth ) {
+	
+	// Mesh refinement - Higher-level function
+	
+	// Each triangle is subdivided into 4 smaller ones - Lower-level recursive function
+	
+	// Vertices are duplicated, whenever needed !!
+	
+	// recursionDepth controls the final number of triangles and vertices 
+    
+    var origArrayLength = coordsArray.length;
+
+    // Copying
+    
+    var origCoords = coordsArray.slice();
+    
+    var origColors = colorsArray.slice();
+    
+    // Clearing the arrays
+    
+    coordsArray.splice( 0, origArrayLength );
+    
+    colorsArray.splice( 0, origArrayLength );
+    
+    var origIndex;
+    
+    // Each triangle is recursively subdivided into 4 triangles
+    
+    // Iterate through the original triangular faces
+    
+    for( origIndex = 0; origIndex < origArrayLength; origIndex += 9 )
+    {
+        /* Call the recursive subdivision function */
+        
+        recSubdivisionMidPoint( origCoords.slice( origIndex, origIndex + 3 ),
+								origCoords.slice( origIndex + 3, origIndex + 6 ),
+								origCoords.slice( origIndex + 6, origIndex + 9 ),
+								origColors.slice( origIndex, origIndex + 3 ),
+								origColors.slice( origIndex + 3, origIndex + 6 ),
+								origColors.slice( origIndex + 6, origIndex + 9 ),
+								coordsArray,
+								colorsArray,
+								recursionDepth );
+    }
+}
+
+//----------------------------------------------------------------------------
+////  Recursive triangle subdivision, using the triangle centroid
+//
+
+function recSubdivisionCentroid( v1, v2, v3, 
+								 c1, c2, c3,
+								 coordsArray,
+								 colorsArray,
+								 recursionDepth ) {
+
+	// Recursive centroid subdivision of one triangle
+	
+	if( recursionDepth == 0 ) {
+		
+		// Storing coordinates and colors in the destination arrays
+ 
+        coordsArray.push( v1[0], v1[1], v1[2] );
+		
+		coordsArray.push( v2[0], v2[1], v2[2] );
+		
+		coordsArray.push( v3[0], v3[1], v3[2] );
+		
+		colorsArray.push( c1[0], c1[1], c1[2] );
+		
+		colorsArray.push( c2[0], c2[1], c2[2] );
+		
+		colorsArray.push( c3[0], c3[1], c3[2] );	    
+	}
+	else {
+		
+		// Compute the centroids and proceed recursively
+		
+        var centroid = computeCentroid( v1, v2, v3 );
+        
+        var colorCentroid = computeCentroid( c1, c2, c3 );
+        
+        // recursive calls
+
+        recSubdivisionCentroid( v1, v2, centroid, c1, c2, colorCentroid,
+                                coordsArray, colorsArray, recursionDepth - 1 );
+        recSubdivisionCentroid( v2, v3, centroid, c2, c3, colorCentroid,
+                                coordsArray, colorsArray, recursionDepth - 1 );
+        recSubdivisionCentroid( v3, v1, centroid, c3, c1, colorCentroid,
+                                coordsArray, colorsArray, recursionDepth - 1 );
+	}
+	
+}
+
+//----------------------------------------------------------------------------
+
+function centroidRefinement( coordsArray, 
+						     colorsArray, 
+							 recursionDepth ) {
+	
+	// Mesh refinement - Higher-level function
+	
+	// Each triangle is subdivided into 3 smaller ones - Lower-level recursive function
+	
+	// Vertices are duplicated, whenever needed !!
+	
+	// recursionDepth controls the final number of triangles and vertices 
+    
+    var origArrayLength = coordsArray.length;
+
+    // Copying
+    
+    var origCoords = coordsArray.slice();
+    
+    var origColors = colorsArray.slice();
+    
+    // Clearing the arrays
+    
+    coordsArray.splice( 0, origArrayLength );
+    
+    colorsArray.splice( 0, origArrayLength );
+    
+    var origIndex;
+    
+    // Each triangle is recursively subdivided into 4 triangles
+    
+    // Iterate through the original triangular faces
+    
+    for( origIndex = 0; origIndex < origArrayLength; origIndex += 9 )
+    {
+        /* Call the recursive subdivision function */
+        
+        recSubdivisionCentroid( origCoords.slice( origIndex, origIndex + 3 ),
+								origCoords.slice( origIndex + 3, origIndex + 6 ),
+								origCoords.slice( origIndex + 6, origIndex + 9 ),
+								origColors.slice( origIndex, origIndex + 3 ),
+								origColors.slice( origIndex + 3, origIndex + 6 ),
+								origColors.slice( origIndex + 6, origIndex + 9 ),
+								coordsArray,
+								colorsArray,
+								recursionDepth );
+    }
+}
+
+//----------------------------------------------------------------------------
+//
+//  Moving vertices to the spherical surface of radius 1
+//
+
+function moveToSphericalSurface( coordsArray ) {
+	// Each vertex is moved to the spherical surface of radius 1
+    // and centered at (0,0,0)
+    
+    // This is done by handling each vertex as if it were a prosition vector,
+    // and normalizing
+	
+    var origArrayLength = coordsArray.length;
+    var origCoords = coordsArray.slice();
+
+	for (i = 0; i < origArrayLength; i += 3) {
+		var v = origCoords.slice(i, i + 3);
+		
+		normalize(v);
+
+		coordsArray[i] = v[0];
+		coordsArray[i + 1] = v[1];
+		coordsArray[i + 2] = v[2];;
+	}
+}
